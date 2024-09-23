1c0a8df2d822a1d44bf1a7ea299242aa48033759
Daniel Grose <31761607+grosed@users.noreply.github.com>
2024-09-23 11:34:55 +0100

    Update __init__.py

--- package/capa_online/__init__.py
+++ package/capa_online/__init__.py
@@ -1,23 +1,23 @@
 
-from ./utils import compose
-from ./utils import class_sumstats
-from ./utils import class_normal_mean_segment_cost
-from ./utils import class_right_cost
-from ./utils import class_left_cost
-from ./utils import class_left_right_cost
-from ./utils import class_summed_cost
-from ./utils import split
-from ./utils import scale
-from ./utils import class_ordered_function
-from ./utils import normal_mean
-from ./utils import op_cpts
-from ./utils import op
-from ./utils import scale_F
-from ./utils import changepoint # probably do not need this
+from .utils import compose
+from .utils import class_sumstats
+from .utils import class_normal_mean_segment_cost
+from .utils import class_right_cost
+from .utils import class_left_cost
+from .utils import class_left_right_cost
+from .utils import class_summed_cost
+from .utils import split
+from .utils import scale
+from .utils import class_ordered_function
+from .utils import normal_mean
+from .utils import op_cpts
+from .utils import op
+from .utils import scale_F
+from .utils import changepoint # probably do not need this
 
-from ./capa import capa_normal_mean
-from ./capa import capa_cpts
-from ./capa import capa
+from .capa import capa_normal_mean
+from .capa import capa_cpts
+from .capa import capa
 
 
 
